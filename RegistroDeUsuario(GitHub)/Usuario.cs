7577e62a9c75e961d9b14127e0de5bc7700c7648
Bryan Peña <bryan15suco@hotmail.com>
2022-03-11 20:15:10 -0500

    se creo la clase usuario

---
+++ RegistroDeUsuario(GitHub)/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDeUsuario_GitHub_
+{
+    public class Usuario
+    {
+        string Nombre { set; get; }
+        string Apellido { set; get; }
+        int Edad { set; get; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string nombre, string apellido, int edad)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+        }
+    }
+}
